4ed76cf79a0bc441876a83a3098baa8c15881bcc
Arzu YILMAZ <arzuyilmaz@Arzus-MacBook-Pro-2.local>
2023-04-24 21:26:29 +0200

    first commit

---
+++ lib/story.dart
@@ -0,0 +1,13 @@
+
+class Story {
+
+  String storyTitle = '';
+  String choice1 = '';
+  String choice2 = '';
+
+  //Constructor for this class to be able to initialise the properties created in step 3.
+  Story({required this.storyTitle, required this.choice1, required this.choice2});
+
+}
+
+
